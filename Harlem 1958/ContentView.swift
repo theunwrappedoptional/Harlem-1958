50ef298ad90e8bf95e62245f5c1caf0ff7c94c1e
Manhattan <carra.rules@gmail.com>
2025-01-31 10:20:25 +0100

    Initial Commit

---
+++ Harlem 1958/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Harlem 1958
+//
+//  Created by Manhattan on 31/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Harlem 1958/Harlem_1958App.swift
@@ -0,0 +1,17 @@
+//
+//  Harlem_1958App.swift
+//  Harlem 1958
+//
+//  Created by Manhattan on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Harlem_1958App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
