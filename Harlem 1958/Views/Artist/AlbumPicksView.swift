5620b46e0f09136ad37a8acea6cb99cd6fa54b02
Manhattan <carra.rules@gmail.com>
2025-02-26 11:50:59 +0100

    Fix AlbumPicksView with new assets, add images to artists.json

--- Harlem 1958/Views/Artist/AlbumPicksView.swift
+++ Harlem 1958/Views/Artist/AlbumPicksView.swift
@@ -22,15 +22,11 @@ struct AlbumPicksView: View {
                 HStack {
                     ForEach(albums, id: \.self) { album in
                         ZStack(){
-                            //FIXME: The image definition seems too low
-                            AsyncImage(url: URL(string: "https://img.icons8.com/ios-filled/100/music-record.png")) { image in
-                                image
-                                    .resizable()
-                                    .opacity(0.2)
-                            } placeholder: {
-                                Color.black.opacity(0)
-                            }
-                            .frame(width: .infinity, height: .infinity,alignment: .center)
+                            
+                            Image("music-record")
+                                .resizable()
+                                .frame(width: 150, height: 150,alignment: .center)
+                                .opacity(0.5)
 
                             Text(album)
                                 .multilineTextAlignment(.center)
